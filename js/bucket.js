533f8429d270b454d54eefd37d4f3665c6379c30
yedantao <32655777+yedantao@users.noreply.github.com>
2017-12-14 14:05:05 -0500

    final web

---
+++ js/bucket.js
@@ -0,0 +1,11 @@
+
+$('.autoplay').slick({
+  slidesToShow: 4,
+ slidesToScroll: 1,
+  autoplay: true,
+   autoplaySpeed: 2000,
+ });
+//
+ $("#tyd1").click(function(){
+   $(".stuff").toggle();
+ });
